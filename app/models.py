4ea0b38dc2c06317ee2d2d2963d91e0688d0d379
Marat Shakirov <marat@shakirov.name>
2024-11-27 17:41:59 +0300

    Some updates

--- app/models.py
+++ app/models.py
@@ -1,14 +1,28 @@
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
-from .database import Base
+from app.database import Base
 
 
 class Task(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, index=True)
+    task_info = Column(String, index=True)
+    datetime_to_do = Column(String, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    datetime_to_do = Column(DateTime(timezone=True), nullable=False)
-    task_info = Column(String, nullable=False)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="tasks")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    tasks = relationship("Task", back_populates="owner")
